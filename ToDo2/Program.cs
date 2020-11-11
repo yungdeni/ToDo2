ea7423cce3602c1a453f32191989bbbb77d66dc3
yungdeni <asertyx@gmail.com>
2020-11-11 13:16:25 +0100

    fas 4

--- ToDo2/Program.cs
+++ ToDo2/Program.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 namespace ToDo2
 {
+    /// <summary>
+    /// Represents an entry in a todolist
+    /// </summary>
     class Activity
     {
         string date;
@@ -25,6 +28,10 @@ namespace ToDo2
                 {
                     status = "*";
                 }
+                else
+                {
+                    Console.WriteLine("Status kan bara vara väntande, pågående eller avklarad");
+                }
             }
             get { return this.status; }
         }
@@ -40,10 +47,17 @@ namespace ToDo2
             status = "v";
             title = inputTitle;
         }
+        /// <summary>
+        /// Outputs the class fields into a '#' seperated format for saving
+        /// </summary>
+        /// <returns>A string</returns>
         public override string ToString()
         {
             return String.Format("{0}#{1}#{2}", date, status, title);
         }
+        /// <summary>
+        /// Prints the class fields date, status and title
+        /// </summary>
         public void Print()
         {
             Console.WriteLine("{0,6} {1} {2}", date, status, title);
@@ -58,12 +72,13 @@ namespace ToDo2
             string currentPath = "";
             List<Activity> todoList = new List<Activity>();
             Console.WriteLine("Welcome to todo");
+            // COMMAND PROMPT
             do
             {
-                
                 Console.Write(">");
                 command = Console.ReadLine();
                 commandWord = command.Split(' ');
+                //CommandWord[0] is the name of the command
                 if (commandWord[0] == "quit")
                 {
                     Console.WriteLine("Bye");
@@ -74,11 +89,7 @@ namespace ToDo2
                 }
                 else if (commandWord[0] == "find")
                 {
-                    string[] dirs = Directory.GetFiles(@"c:\users\frage", "*.lis");
-                    foreach (string dir in dirs)
-                    {
-                        Console.WriteLine(dir);
-                    }
+                    FindFiles();
                 }
                 else if (commandWord[0] == "save")
                 {
@@ -90,50 +101,187 @@ namespace ToDo2
                 }
                 else if (commandWord[0] == "move")
                 {
-                    int index = int.Parse(commandWord[1]) - 1;
-                    Activity temp = todoList[index];
-                    if (commandWord[2] == "up")
-                    {
-                        todoList[index] = todoList[index - 1];
-                        todoList[index - 1] = temp;
-                    }
-                    else if (commandWord[2] == "down")
-                    {
-                        todoList[index] = todoList[index + 1];
-                        todoList[index + 1] = temp;
-                    }
+                    MoveActivity(commandWord, todoList);
                 }
                 else if (commandWord[0] == "delete")
                 {
-                    int index = int.Parse(commandWord[1]) - 1;
-                    todoList.RemoveAt(index);
+                    DeleteActivity(commandWord, todoList);
                 }
                 else if (commandWord[0] == "add")
                 {
-                    todoList.Add(new Activity(commandWord[1], commandWord[2]));
+                    AddActivity(commandWord, todoList);
                 }
                 else if (commandWord[0] == "set")
                 {
-                    int index = int.Parse(commandWord[1]) - 1;
-                    todoList[index].Status = commandWord[2];
-                }
+                    SetStatus(commandWord, todoList);
+                }              
             } while (commandWord[0] != "quit");
         }
+        /// <summary>
+        /// A static method for adding activities to a list
+        /// </summary>
+        /// <param name="commandWord">stringarray containing the values to be added</param>
+        /// <param name="todoList">The list we add Activitys too</param>
+        private static void AddActivity(string[] commandWord, List<Activity> todoList)
+        {
+            if (commandWord.Length < 3)
+            {
+                Console.WriteLine("Skriv in ett datum");
+                string inputDate = Console.ReadLine();
+                Console.WriteLine("Skriv in en aktivitet");
+                string inputTitle = Console.ReadLine();
+                todoList.Add(new Activity(inputDate, inputTitle));
+            }
+            else
+            {
+                todoList.Add(new Activity(commandWord[1], commandWord[2]));
+            }
+        }
+        /// <summary>
+        /// A static method to remove activities from a list
+        /// </summary>
+        /// <param name="commandWord">A string array containing the index into the list</param>
+        /// <param name="todoList">The list we remove values from</param>
+        private static void DeleteActivity(string[] commandWord, List<Activity> todoList)
+        {
+            try
+            {
+                int index = int.Parse(commandWord[1]) - 1;
+                todoList.RemoveAt(index);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Du måste ange ett heltal");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Du försökte ta bort en aktivitet utanför listan");
+            }
+        }
+        /// <summary>
+        /// A static method to move activites in the list
+        /// </summary>
+        /// <param name="commandWord">A string array containing the index and direction</param>
+        /// <param name="todoList">The list we move values in</param>
+        private static void MoveActivity(string[] commandWord, List<Activity> todoList)
+        {
+            //index-1 because the array is 0-indexed
+            try
+            {
+                int index = int.Parse(commandWord[1]) - 1;
+                Activity temp = todoList[index];
+                if (commandWord[2] == "up")
+                {
+                    //swap the order of the activity with the previous one
+                    todoList[index] = todoList[index - 1];
+                    todoList[index - 1] = temp;
+                }
+                else if (commandWord[2] == "down")
+                {
+                    //swap the order of the activity with the next one
+                    todoList[index] = todoList[index + 1];
+                    todoList[index + 1] = temp;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Du måste ange ett heltal");
 
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Flytten misslyckades, du angav en aktivitet" +
+                    " utanför listan eller försökte flytta den utanför");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Flytten misslyckades, du angav en aktivitet" +
+                    " utanför listan");
+            }
+        }
+        /// <summary>
+        /// A static method to find the path to files ending with .lis in the users home directory
+        /// </summary>
+        private static void FindFiles()
+        {
+            //Find all files in the homedirectory with the extension .lis
+            try
+            {
+                string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string[] dirs = Directory.GetFiles(homeDirectory, "*.lis");
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        /// <summary>
+        /// A static method to change the status of an activity
+        /// </summary>
+        /// <param name="commandWord">A string array containing the index and status to change</param>
+        /// <param name="todoList">A list of activities</param>
+        private static void SetStatus(string[] commandWord, List<Activity> todoList)
+        {
+            try
+            {
+                int index = int.Parse(commandWord[1]) - 1;
+                todoList[index].Status = commandWord[2];
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ange ett heltal");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Skriv t.ex set 1 avklarad");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Den aktiviteten finns inte");
+            }
+        }
+        /// <summary>
+        /// A static method to load activities from a textfile into a list
+        /// </summary>
+        /// <param name="commandWord">A string array containing the path</param>
+        /// <param name="currentPath">A string that saves the path used for later when saving</param>
+        /// <param name="todoList">A list containing activities</param>
         private static void LoadTodolist(string[] commandWord, out string currentPath, out List<Activity> todoList)
         {
-            List<Activity> temp = new List<Activity>();
-            currentPath = commandWord[1];
-            string[] lines = File.ReadAllLines(currentPath);
-            foreach (string line in lines)
+            if (commandWord.Length == 1)
             {
-                string[] i = line.Split('#');
-                temp.Add(new Activity(i[0], i[1], i[2]));
-
+                Console.WriteLine("Ange filsökväg");
+                currentPath = Console.ReadLine();
+            }
+            else
+            {
+                currentPath = commandWord[1];
+            }
+            List<Activity> temp = new List<Activity>();
+            try
+            {
+                string[] lines = File.ReadAllLines(currentPath);
+                foreach (string line in lines)
+                {
+                    string[] i = line.Split('#');
+                    temp.Add(new Activity(i[0], i[1], i[2]));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
             todoList = temp;
         }
-
+        /// <summary>
+        /// Prints out the activities from a list in a nice format
+        /// </summary>
+        /// <param name="commandWord">String array containing the arguments for different formats</param>
+        /// <param name="todoList">The list containing the activities</param>
         private static void PrintTodoList(string[] commandWord, List<Activity> todoList)
         {
             Console.WriteLine("N  datum  S rubrik");
@@ -170,7 +318,12 @@ namespace ToDo2
             }
             Console.WriteLine("-------------------------------------------");
         }
-
+        /// <summary>
+        /// A static method that saves a list of activites to a file
+        /// </summary>
+        /// <param name="commandWord">string array containing userinputed path</param>
+        /// <param name="currentPath">The path that the file was loaded from</param>
+        /// <param name="todoList">A list containing the activites to be saved</param>
         private static void SaveToFile(string[] commandWord, string currentPath, List<Activity> todoList)
         {
             string[] toSave = new string[todoList.Count()];
@@ -178,13 +331,20 @@ namespace ToDo2
             {
                 toSave[i] = (todoList[i].ToString());
             }
-            if (commandWord.Length == 1)
+            try
             {
-                File.WriteAllLines(currentPath, toSave);
+                if (commandWord.Length == 1)
+                {
+                    File.WriteAllLines(currentPath, toSave);
+                }
+                else
+                {
+                    File.WriteAllLines(commandWord[1], toSave);
+                }
             }
-            else
+            catch (Exception e)
             {
-                File.WriteAllLines(commandWord[1], toSave);
+                Console.WriteLine("Could not save file " + e.Message);
             }
         }
     }
